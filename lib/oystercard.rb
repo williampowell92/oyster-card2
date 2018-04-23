58e3d2470a5ae9ca501389078f998483d739848c
Ellie Keen <elliekeen95@gmail.com>
2018-04-23 17:30:49 +0100

    Created deduct method and min_limit? method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,22 +1,32 @@
+MAX_LIMIT = 90
+
 class Oystercard
   attr_reader :balance
-
-  MAX_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    @amount = amount
+    @addition = amount
     raise "Cannot top up as balance exceeds maximum limit." if max_limit?
-    puts "Your total balance is £#{@balance += @amount}."
+    "Your total balance is £#{@balance += @addition}."
+  end
+
+  def deduct(amount)
+    @deduction = amount
+    raise "Cannot travel. Insufficent funds." if min_limit?
+    "Your total balance is £#{@balance -= @deduction}"
   end
 
 private
 
   def max_limit?
-    @balance + @amount > MAX_LIMIT
+    @balance + @addition > MAX_LIMIT
+  end
+
+  def min_limit?
+    @balance - @deduction < 0
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,7 @@ describe Oystercard do
  describe '#balance' do
    it 'should return the balance' do
    expect(oystercard.balance).to eq 0
-end
+   end
 end
 
 describe '#top_up' do
@@ -20,6 +20,17 @@ describe '#top_up' do
 
   it 'should raise error if total exceeds max limit' do
     expect { oystercard.top_up(100) }.to raise_error("Cannot top up as balance exceeds maximum limit.")
+  end
+end
+
+describe '#deduct' do
+  it 'should deduct an amount from our balance and return correct amount' do
+    oystercard.instance_variable_set(:@balance, 20)
+    expect { oystercard.deduct(10) }.to change { oystercard.balance }.by(-10)
+  end
+
+  it 'should raise error if balance is below 0' do
+    expect { oystercard.deduct(10) }.to raise_error("Cannot travel. Insufficent funds.")
   end
 end
 
