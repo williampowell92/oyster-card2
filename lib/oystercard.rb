4de781f0b1d96c776f7a708f92c468b2a692ec2d
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-04-23 16:54:04 +0100

    set maximum limit and added error message for exceeding limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
 
+  MAX_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Cannot top up as balance exceeds maximum limit." if @balance + amount > MAX_LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,21 +1,26 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:oystercard) { Oystercard.new }
+
  describe '#balance' do
    it 'should return the balance' do
-   expect(subject.balance).to eq 0
+   expect(oystercard.balance).to eq 0
 end
 end
 
 describe '#top_up' do
   it 'should respond to top_up method' do
-    expect(subject).to respond_to(:top_up).with(1).argument
+    expect(oystercard).to respond_to(:top_up).with(1).argument
   end
 
   it 'should return correct balance when topped up' do
-    expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+    expect { oystercard.top_up(10) }.to change { oystercard.balance }.by(10)
   end
 
+  it 'should raise error if total exceeds max limit' do
+    expect { oystercard.top_up(100) }.to raise_error("Cannot top up as balance exceeds maximum limit.")
+  end
 end
 
 end
