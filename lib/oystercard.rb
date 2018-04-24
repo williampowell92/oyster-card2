2b8471bd1ec4a4c992a9e3a29f1ffffdc70c2979
Igor Ryabchuk <riabtchuk@gmail.com>
2018-04-24 16:55:47 +0100

    changed list_of_journeys to an array

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,7 +7,7 @@ class Oystercard
 
   def initialize
     @balance = 0
-    @list_of_journeys = {}
+    @list_of_journeys = []
   end
 
   def top_up(amount)
@@ -23,7 +23,7 @@ class Oystercard
   def touch_out(station)
     deduct(MINIMUM_CHARGE)
     @exit_station = station.__id__
-    @list_of_journeys[Time.now] = { entry_station: @entry_station, exit_station:@exit_station }
+    @list_of_journeys << { entry_station: @entry_station, exit_station:@exit_station }
     @entry_station = nil
     @exit_station = nil
   end
