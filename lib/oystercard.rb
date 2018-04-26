666885e147e143afa0e8fa59e7b85f50fe3cacde
William Powell <william.powell92@gmail.com>
2018-04-26 12:12:44 +0100

    Refactored oyster card tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -10,6 +10,7 @@ class Oystercard # creates a class called OysterCard
   def initialize # method that runs on instance creation
     @balance = 0 # sets instance variable balance to 0
     @journeys = [] # sets instance variable journeys to []
+    @journey = {}
   end
 
   def top_up(amount) # method that takes one argument
@@ -18,24 +19,25 @@ class Oystercard # creates a class called OysterCard
   end
 
   def touch_in(entry_station) # method that takes one argument
-    raise 'Insufficient funds for a journey' if insufficient_funds? #
-    @entry_station = entry_station
+    raise 'Insufficient funds for a journey' if insufficient_funds? # raises error if insufficient funds
+    @journey[:entry_station] = entry_station
   end
 
-  def touch_out(exit_station)
-    deduct(MINIMUM_CHARGE)
-    @journeys << { @entry_station => exit_station }
-    @entry_station = nil
+  def touch_out(exit_station) # method that takes one argument
+    deduct(MINIMUM_CHARGE) # reduces balance by amount
+    @journey[:exit_station] = exit_station # sets the exit station in journey to exit station
+    @journeys << @journey # saves the journey into journeys
+    @journey = {} # resets the journey hash
   end
 
   def in_journey?
-      !@entry_station.nil?
+    !!@journey[:entry_station] && @journey[:exit_station].nil?
   end
 
 private
 
   def deduct(amount)
-    "Your total balance is £#{@balance -= amount}"
+    @balance -= amount
   end
 
   def exceeds_limit?(amount)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -33,52 +33,48 @@ describe Oystercard do
       expect { subject.touch_in(entry_station) }.to raise_error 'Insufficient funds for a journey'
     end
 
-  context 'changes caused by touch_in' do
-    before(:each) do
-      subject.instance_variable_set(:@balance, 20)
-    end
-    it 'should change in_journey? on touch_in' do
-      subject.touch_in(entry_station)
-      expect(subject).to be_in_journey
+    context 'changes caused by touch_in' do
+      before(:each) do
+        subject.instance_variable_set(:@balance, 20)
+      end
+
+      it 'should change in_journey? on touch_in' do
+        subject.touch_in(entry_station)
+        expect(subject).to be_in_journey
+      end
     end
   end
-end
-  describe '#touch_out' do
 
+  describe '#touch_out' do
     it 'should deduct minimum fare upon #touch_out' do
       subject.instance_variable_set(:@balance, 20)
       subject.touch_in(entry_station)
       expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-1)
     end
+
     context 'touch out affects in_journey? method' do
       before(:each) do
         subject.instance_variable_set(:@balance, 20)
         subject.touch_in(entry_station)
       end
+
       it 'should store exit_station on touch_out' do
-        baker_street = "Baker Street"
-        marylebone = "Marylebone"
-        subject.touch_in(baker_street)
-        subject.touch_out(marylebone)
-        expect(subject.journeys.last[baker_street]).to eq marylebone
+        subject.touch_in(entry_station)
+        subject.touch_out(exit_station)
+        expect(subject.journeys.last[:exit_station]).to eq exit_station
       end
+
       it 'should change in_journey? on touch_out' do
         subject.touch_out(exit_station)
         expect(subject).not_to be_in_journey
       end
     end
-  end
 
-    context 'touching in and out' do
-      before(:each) do
-        subject.instance_variable_set(:@balance, 20)
-        subject.touch_in(entry_station)
-        subject.touch_out(exit_station)
-      end
-
-      it 'should store one journey' do
-        expect(subject.journeys.length).to eq 1
-      end
+    it 'should store one journey' do
+      subject.instance_variable_set(:@balance, 20)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.journeys.length).to eq 1
     end
-
+  end
 end
