260f9bc70e1c02f2a4404e5bcb1a55c4e82ce241
William Powell <william.powell92@gmail.com>
2018-04-26 15:49:03 +0100

    Updated oystercard to use journey class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,58 +1,48 @@
-# holds balance and allows you to manipulate it
+require 'journey'
 
-class Oystercard # creates a class called OysterCard
-  MAX_LIMIT = 90 # creates a constant called MAX_LIMIT
-  MINIMUM_BALANCE = 1 # creates a constant called MINIMUM_BALANCE
-  MINIMUM_CHARGE = 1 # creates a constant called MINIMUM_CHARGE
+class Oystercard
+  MAX_LIMIT = 90
+  MINIMUM_CHARGE = 1
 
-  attr_reader :balance, :journeys # gives access to balance and journeys
+  attr_reader :balance, :journeys
 
-  def initialize # method that runs on instance creation
-    @balance = 0 # sets instance variable balance to 0
-    @journeys = [] # sets instance variable journeys to []
-    @journey = {}
+  def initialize
+    @balance = 0
+    @journeys = []
   end
 
-  def top_up(amount) # method that takes one argument
-    raise "Cannot top up as balance exceeds maximum limit." if exceeds_limit?(amount) # raises error if amount exceeds card limit
-    @balance += amount # increases balance by amount
+  def top_up(amount)
+    raise "Cannot top up as balance exceeds maximum limit." if exceeds_limit?(amount)
+    @balance += amount
   end
 
-  def touch_in(entry_station) # method that takes one argument
-    raise 'Insufficient funds for a journey' if insufficient_funds? # raises error if insufficient funds
-    @journey[:entry_station] = entry_station
+  def touch_in(entry_station)
+    raise 'Insufficient funds for a journey' if insufficient_funds?
+    @journey = Journey.new
+    @journey.start(entry_station)
   end
 
-  def touch_out(exit_station) # method that takes one argument
-    deduct(MINIMUM_CHARGE) # reduces balance by amount
-    @journey[:exit_station] = exit_station # sets the exit station in journey to exit station
-    save_journey # calls method to save journey hash into journeys array
-    reset_journey # resets journey to a new hash
-  end
-
-  def in_journey? # method takes no arguments
-    !!@journey[:entry_station] && @journey[:exit_station].nil? # returns true if entry station is not nil and exit station is nil
+  def touch_out(exit_station)
+    deduct(MINIMUM_CHARGE)
+    @journey.finish(exit_station)
+    save_journey
   end
 
 private
 
-  def deduct(amount) # method takes one argument
-    @balance -= amount # deducts amount from balance
+  def deduct(amount)
+    @balance -= amount
   end
 
   def save_journey
-    @journeys << @journey # saves the journey into journeys
-  end
-
-  def reset_journey
-    @journey = {} # resets the journey hash
+    @journeys << @journey
   end
 
   def exceeds_limit?(amount)
-    @balance + amount > MAX_LIMIT # checks if an addition will exceed the cards limit
+    @balance + amount > MAX_LIMIT
   end
 
   def insufficient_funds?
-    balance < MINIMUM_BALANCE # checks if the card has insufficient funds
+    balance < MINIMUM_CHARGE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,7 @@ describe Oystercard do
   let(:oystercard) { Oystercard.new }
   let(:entry_station) { double :entry_station }
   let(:exit_station) { double :exit_station}
+  let(:minimum_charge) { Oystercard::MINIMUM_CHARGE }
 
   describe '#balance' do
     it 'should return the balance' do
@@ -32,46 +33,24 @@ describe Oystercard do
     it 'should raise an error message if balance on #touch_in is less than £1' do
       expect { subject.touch_in(entry_station) }.to raise_error 'Insufficient funds for a journey'
     end
-
-    context 'changes caused by touch_in' do
-      before(:each) do
-        subject.instance_variable_set(:@balance, 20)
-      end
-
-      it 'should change in_journey? on touch_in' do
-        subject.touch_in(entry_station)
-        expect(subject).to be_in_journey
-      end
-    end
   end
 
   describe '#touch_out' do
     it 'should deduct minimum fare upon #touch_out' do
-      subject.instance_variable_set(:@balance, 20)
+      subject.top_up(minimum_charge)
       subject.touch_in(entry_station)
       expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-1)
     end
 
-    context 'touch out affects in_journey? method' do
-      before(:each) do
-        subject.instance_variable_set(:@balance, 20)
-        subject.touch_in(entry_station)
-      end
-
-      it 'should store exit_station on touch_out' do
-        subject.touch_in(entry_station)
-        subject.touch_out(exit_station)
-        expect(subject.journeys.last[:exit_station]).to eq exit_station
-      end
-
-      it 'should change in_journey? on touch_out' do
-        subject.touch_out(exit_station)
-        expect(subject).not_to be_in_journey
-      end
+    it 'should store exit_station on touch_out' do
+      subject.top_up(minimum_charge)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.journeys.last.exit_station).to eq exit_station
     end
 
     it 'should store one journey' do
-      subject.instance_variable_set(:@balance, 20)
+      subject.top_up(minimum_charge)
       subject.touch_in(entry_station)
       subject.touch_out(exit_station)
       expect(subject.journeys.length).to eq 1
